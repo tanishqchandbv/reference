20e3b5b083c1feef906b74e297515382a9e26b92
tanishqchandbv <154722447+tanishqchandbv@users.noreply.github.com>
2024-02-11 13:50:35 +0530

    Delete testnew.php

--- testnew.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "Name: ". $_REQUEST['fname'] . "<br>";
-echo "Age: " . $_REQUEST['age'] . "<br>";
-echo "Gender: " . $_REQUEST['gender']. "<br>";
-$course = $_REQUEST['course'];
-echo "Course: " . implode($course). "<br>";
-echo "Mobile: " . $_REQUEST['mob']. "<br>";
-echo "Email: " . $_REQUEST['email']. "<br>";
-?>
